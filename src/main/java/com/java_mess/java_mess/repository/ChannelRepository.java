41d17175ae166ed7624ce6d77b8ffb915c1a0300
Khanh-21522203 <21522203@gm.uit.edu.vn>
2025-02-03 22:12:31 +0700

    add logic and controller of Channel

--- src/main/java/com/java_mess/java_mess/repository/ChannelRepository.java
+++ src/main/java/com/java_mess/java_mess/repository/ChannelRepository.java
@@ -1,9 +1,10 @@
 package com.java_mess.java_mess.repository;
 
-import java.nio.channels.Channel;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.java_mess.java_mess.model.Channel;
 
 public interface ChannelRepository extends JpaRepository<Channel, String> {
     Optional<Channel> findByAppIdAndClientReferenceId(String appId, String clientReferenceId);
